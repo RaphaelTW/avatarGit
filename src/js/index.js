31b05aae07bcb952b835ac19ae86950abdeed017
Raphael Silva | Via Expressa Soluções Logísticas <raphael.silva@viaexpressa.com>
2024-07-17 17:03:21 -0300

    fist commit

---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+Swal.fire({
+  title: "Escreva o seu nome de usuário do GitHub",
+  input: "text",
+  inputAttributes: {
+    autocapitalize: "off"
+  },
+  showCancelButton: true,
+  confirmButtonText: "Buscar",
+  showLoaderOnConfirm: true,
+  preConfirm: async (login) => {
+    try {
+      const githubUrl = `
+        https://api.github.com/users/${login}
+      `;
+      const response = await fetch(githubUrl);
+      if (!response.ok) {
+        return Swal.showValidationMessage(`
+          ${JSON.stringify(await response.json())}
+        `);
+      }
+      return response.json();
+    } catch (error) {
+      Swal.showValidationMessage(`
+        Request failed: ${error}
+      `);
+    }
+  },
+  allowOutsideClick: () => !Swal.isLoading()
+}).then((result) => {
+  if (result.isConfirmed) {
+    Swal.fire({
+      title: `${result.value.login}'s avatar`,
+      imageUrl: result.value.avatar_url
+    });
+  }
+});
